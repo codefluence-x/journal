f41025bd1cf945e9bed56354c474ee254d3cbdb3
insomnius <aliana.arib@gmail.com>
2019-02-16 13:22:54 +0700

    All of Journal basic feature is complete

--- journal.go
+++ journal.go
@@ -1,49 +1,87 @@
-package journall
+package journal
+
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var trackId, _ = uuid.NewV4()
 
 type Journal interface {
-	// Info(msg string) JournalInfo
-	// InfoWithTags(msg string, tags ...string) JournalInfo
-	// InfoWithName(name, msg string) JournalInfo
+	// Sometimes log need tags to be grouped or just easier search in log architecture
+	SetTags(tags ...string) Journal
+	// Add custom field
+	AddField(field string, value interface{}) Journal
 
-	// Warning(msg string) JournalWarning
-	// WarningWithTags(msg string, tags ...string) JournalWarning
-	// WarningWithName(name, msg string) JournalWarning
-
-	// Error(msg string, err error) JournalError
-	// ErrorWithTags(msg string, err error, tags ...string) JournalError
-	// ErrorWithName(name, msg string, err error) JournalError
+	// Print the log
+	Log()
 }
 
-type JournalBase interface {
-	// Name() string
-	// Tags() []string
-	// Field(field string, value interface{}) JournalBase
-
-	// LogType() string
-
-	// SetName(name string) JournalBase
-	// SetTags(tags ...string) JournalBase
-
-	// Log()
-}
-
-type JournalInfo interface {
-	JournalBase
-}
-
-type JournalWarning interface {
-	JournalBase
-}
-
-type JournalError interface {
-	JournalBase
-}
-
-type journal struct {
+type journalLogger struct {
 	msg    string
-	err    string
+	level  string
 	errRaw error
-	name   string
 	tags   []string
-	log    string
+	fields map[string]interface{}
+}
+
+func baseJournal(msg, level string) *journalLogger {
+	return &journalLogger{msg: msg, level: level, fields: map[string]interface{}{}}
+}
+
+// New Journal info interface
+func Info(msg string) Journal {
+	return baseJournal(msg, "info")
+}
+
+// New Journal warning interface
+func Warning(msg string) Journal {
+	return baseJournal(msg, "warning")
+}
+
+// New Journal error interface
+func Error(msg string, err error) Journal {
+	base := baseJournal(msg, "error")
+	base.errRaw = err
+	return base
+}
+
+func (j *journalLogger) SetTags(tags ...string) Journal {
+	j.tags = append(j.tags, tags...)
+	return j
+}
+
+func (j *journalLogger) AddField(field string, value interface{}) Journal {
+	j.fields[field] = value
+	return j
+}
+
+func (j *journalLogger) Log() {
+	j.appendAll()
+
+	if j.level == "error" {
+		_, file, no, ok := runtime.Caller(1)
+		if ok {
+			j.fields["caller"] = fmt.Sprintf("%s:%d", file, no)
+		}
+	}
+
+	toBePrint, _ := json.Marshal(j.fields)
+	fmt.Printf("%s\n", toBePrint)
+}
+
+func (j *journalLogger) appendAll() {
+	j.fields["track_id"] = trackId.String()
+	j.fields["message"] = j.msg
+	j.fields["level"] = j.level
+
+	if len(j.tags) > 0 {
+		j.fields["tags"] = j.tags
+	}
+
+	j.fields["timestamp"] = time.Now()
 }
